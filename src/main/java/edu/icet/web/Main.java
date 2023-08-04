40542012380017acd8ff398308b0c4e01b924c13
Rashen <rashen598@gmail.com>
2023-08-04 16:19:34 +0530

    Imported lambok to the project.

---
+++ src/main/java/edu/icet/web/Main.java
@@ -0,0 +1,10 @@
+package edu.icet.web;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Student student = new Student();
+        student.getLastName();
+        student.setLastName("he");
+    }
+}
---
+++ src/main/java/edu/icet/web/Student.java
@@ -0,0 +1,9 @@
+package edu.icet.web;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private String firstName;
+    private String lastName;
+}
